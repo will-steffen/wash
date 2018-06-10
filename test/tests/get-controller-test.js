b1f99c05f221aa2dfe4ca2a09a01ff26e8a342b8
will-steffen <steffen.willian@gmail.com>
2018-06-10 05:50:41 -0300

    scripts http request

---
+++ test/tests/get-controller-test.js
@@ -0,0 +1,18 @@
+let base = require('./wash-test');
+let Method = base.Method;
+let Request = base.Request;
+let Code = base.Code;
+
+// Request(Method.Get, 'get').then(resp => {
+//     console.log(resp);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+
+// Request('completenew', 'get/defined', {data: 'data'}).then(resp => {
+//     console.log(resp);
+// }).catch(err => {
+//     console.log(err);
+// });
---
+++ test/tests/wash-test.js
@@ -0,0 +1,53 @@
+
+const http = require('http');
+
+let Method = {
+    Get: 'get',
+    Post: 'post',
+    Put: 'put',
+    Delete: 'delete',
+    Options: 'options'
+};
+
+let Code = {
+    OK: 'OK',
+    UNAUTHORIZED: 'Unauthorized',
+    FORBIDDEN: 'Forbidden',
+    NOT_FOUND: 'Not Found',
+    ERROR: 'Internal Server Error'
+};
+
+function Resp(code, content) {
+    this.code = code;
+    this.content = content;
+}
+
+function Request(method, route, data) {
+    return new Promise((res, rej) => {
+        let request = http.request({
+            path: '/' + route,
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }, function(response){
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {                
+                res(new Resp(response.statusMessage, body));
+            });
+        });
+        if(data){
+            request.write(JSON.stringify(data));
+        }
+        request.end();
+    });    
+}
+
+
+exports.Method = Method;
+exports.Resp = Resp;
+exports.Request = Request;
+exports.Code = Code;
